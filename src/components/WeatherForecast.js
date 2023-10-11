d11179d610f6d2df2911b09e810929765fa4dc7a
TheWiseSailor <rudasilljay@gmail.com>
2023-10-11 17:09:01 -0400

    simple weather forcast copnverted into react

--- src/components/WeatherForecast.js
+++ src/components/WeatherForecast.js
@@ -1,13 +1,17 @@
 import React from "react";
 
-const WeatherForecast = () => {
-  // You can use state or props to display the 5-day forecast
-  // Implement your forecast rendering logic here
-
+const WeatherForecast = ({ forecastData }) => {
   return (
     <div className="weather-forecast">
       <h4>5-Day Forecast</h4>
-      {/* Display the 5-day forecast here */}
+      {forecastData.map((forecast, index) => (
+        <div key={index} className="forecast-item">
+          <p>Date: {forecast.date}</p>
+          <img src={forecast.icon} alt="Weather Icon" />
+          <p>Temperature: {forecast.temperature}°F</p>
+          <p>Humidity: {forecast.humidity}%</p>
+        </div>
+      ))}
     </div>
   );
 };
