7dab84b42bc52bc3bf10e008f675f338efea9e08
TheWiseSailor <rudasilljay@gmail.com>
2023-10-11 17:07:16 -0400

    might delete this file but for now this is a bug/feature :D since it doesnt ruin anything

---
+++ src/components/SearchedCities.js
@@ -0,0 +1,7 @@
+import React from "react";
+import WeatherSearch from "./WeatherSearch";
+const SearchedCities = ({ cities, onCityClick }) => {
+  return <aside></aside>;
+};
+
+export default SearchedCities;
