b55852db03eca474affb67a9515cde004ed6bb6b
TheWiseSailor <rudasilljay@gmail.com>
2023-10-24 18:33:21 -0400

    placed comments in this file

--- src/components/CurrentWeather.js
+++ src/components/CurrentWeather.js
@@ -3,17 +3,26 @@ import "../components/styles/App.css";
 
 const CurrentWeather = ({ weather }) => {
   return (
-    
-    <div className="flex justify-center items-center ">
-       
+    <div className="flex justify-center items-center">
       <div className="bg-blue-100 text-black p-4 rounded-xl shadow-md text-center">
-      <div className="  text-center text-2xl font-semibold ">Todays Weather
+        {/* Display the "Today's Weather" heading */}
+        <div className="text-center text-2xl font-semibold">Todays Weather</div>
+
+        {/* Display the city name and date */}
         <h2 className="text-2xl font-semibold mt-8">
           {weather.city} (<span className="text-gray-600">{weather.date}</span>)
         </h2>
+
+        {/* Display temperature */}
         <p className="text-lg">Temperature: {weather.temperature}°F</p>
+
+        {/* Display humidity */}
         <p className="text-lg">Humidity: {weather.humidity}%</p>
+
+        {/* Display wind speed */}
         <p className="text-lg mb-2">Wind Speed: {weather.windSpeed} MPH</p>
+
+        {/* Display UV Index with dynamic styling based on its value */}
         <p className="text-lg">
           UV Index:{" "}
           <span
@@ -28,7 +37,6 @@ const CurrentWeather = ({ weather }) => {
             {weather.uvIndex}
           </span>
         </p>
-        </div>
       </div>
     </div>
   );
