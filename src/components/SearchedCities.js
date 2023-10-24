bc6ad13c580f6948d93a43053cb6bed3994c11c7
TheWiseSailor <rudasilljay@gmail.com>
2023-10-24 18:35:53 -0400

    added code on whats happening her aswell

--- src/components/SearchedCities.js
+++ src/components/SearchedCities.js
@@ -4,13 +4,27 @@ const SearchedCities = ({ cities, onCityClick, isDarkMode }) => {
   return (
     <aside>
       <div>
-        <h2 className={` pt-7 text-xl font-semibold ${isDarkMode ? "text-white" : ""}`}>Searched Cities</h2>
+        {
+          /* Display the "Searched Cities" heading with optional white text in dark mode */
+          <h2
+            className={`pt-7 text-xl font-semibold ${
+              isDarkMode ? "text-white" : ""
+            }`}
+          >
+            Searched Cities
+          </h2>
+        }
         <ul>
+          {/*  Map through the list of cities and create a list item for each  */}
           {cities.map((city, index) => (
             <li key={index}>
               <button
+                // When a city button is clicked, call the onCityClick function with the city
                 onClick={() => onCityClick(city)}
-                className={`font-bold hover:underline cursor-pointer rounded-xl pl-2 pr-2 mt-4 ${isDarkMode ? "text-white" : ""}`}
+                // Style the city button with optional white text in dark mode
+                className={`font-bold hover:underline cursor-pointer rounded-xl pl-2 pr-2 mt-4 ${
+                  isDarkMode ? "text-white" : ""
+                }`}
               >
                 {city}
               </button>
