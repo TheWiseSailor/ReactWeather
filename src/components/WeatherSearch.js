3a4478067f15ca65b8123735374ea7e350e02642
TheWiseSailor <rudasilljay@gmail.com>
2023-10-24 18:44:05 -0400

    more commenting

--- src/components/WeatherSearch.js
+++ src/components/WeatherSearch.js
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
 import "../components/styles/App.css";
+
+// Create and append a link element for AOS CSS
 const link = document.createElement("link");
 link.rel = "stylesheet";
 link.href = "https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.css";
 document.head.appendChild(link);
 
+// Create and append a script element for AOS JavaScript
 const script = document.createElement("script");
 script.src = "https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.js";
 document.body.appendChild(script);
@@ -13,15 +16,18 @@ document.body.appendChild(script);
 script.onload = () => {
   window.AOS.init();
 };
+
 const WeatherSearch = ({
   onSearch,
   isSearching,
   isDarkMode,
   setIsDarkMode,
 }) => {
+  // State management using React hooks
   const [city, setCity] = useState("");
   const [searchButtonClicked, setSearchButtonClicked] = useState(false);
 
+  // Function to handle the search for weather data
   const handleSearch = () => {
     if (!searchButtonClicked) {
       setSearchButtonClicked(true);
@@ -30,6 +36,7 @@ const WeatherSearch = ({
     setCity("");
   };
 
+  // Function to determine the CSS class based on searchButtonClicked state
   const getClassName = () => {
     if (searchButtonClicked) {
       return "App flex flex-col items-center justify-center";
@@ -44,6 +51,7 @@ const WeatherSearch = ({
         Welcome to Weather DashBoard
       </div>
       <div className="flex justify-center items-center " data-aos="fade-left">
+        {/* Input field for city search */}
         <input
           type="text"
           placeholder="Search for a city"
@@ -51,6 +59,8 @@ const WeatherSearch = ({
           onChange={(e) => setCity(e.target.value)}
           className="w-60 p-2 border rounded mr-4 text-black"
         />
+
+        {/* Button for initiating the search */}
         <button
           onClick={handleSearch}
           className="bg-blue-500 hover:bg-blue-700 transform transition-transform hover:translate-x-1 hover:bg-green-500 text-white font-bold py-2 px-4 rounded"
